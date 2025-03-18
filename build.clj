9f974b1f6de772fc2304ed3687b80a05662982be
Adrian Smith <smith.adriane@gmail.com>
2025-03-18 14:12:58 -0700

    Bump version to 1.1.

--- build.clj
+++ build.clj
@@ -3,7 +3,7 @@
             [clojure.string :as str]))
 
 (def lib 'com.phronemophobic/vt)
-(def version "1.1-SNAPSHOT")
+(def version "1.1")
 (def class-dir "target/classes")
 (def basis (b/create-basis {:project "deps.edn"}))
 (def jar-file (format "target/%s-%s.jar" (name lib) version))
