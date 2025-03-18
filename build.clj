dcc47262dfbf10d1c9d11503abbd7227d52d17bf
Adrian Smith <smith.adriane@gmail.com>
2025-03-18 13:54:35 -0700

    Add tools.build.

---
+++ build.clj
@@ -0,0 +1,49 @@
+(ns build
+  (:require [clojure.tools.build.api :as b]
+            [clojure.string :as str]))
+
+(def lib 'com.phronemophobic/vt)
+(def version "1.1-SNAPSHOT")
+(def class-dir "target/classes")
+(def basis (b/create-basis {:project "deps.edn"}))
+(def jar-file (format "target/%s-%s.jar" (name lib) version))
+
+(defn clean [_]
+  (b/delete {:path "target"}))
+
+(defn compile [_]
+  #_(b/javac {:src-dirs ["javasrc"]
+            :class-dir class-dir
+            :basis basis
+            ;:javac-opts ["-source" "8" "-target" "8"]
+            }))
+
+(defn jar [opts]
+  (compile opts)
+  (b/write-pom {:class-dir class-dir
+                :lib lib
+                :version version
+                :basis basis
+                :pom-data
+                [[:licenses
+                  [:license
+                   [:name "Apache License, Version 2.0"]
+                   [:url "http://www.apache.org/licenses/LICENSE-2.0"]]]]
+                :src-dirs ["src"]})
+  (b/copy-dir {:src-dirs ["src" "resources"]
+               :target-dir class-dir})
+  (b/jar {:class-dir class-dir
+          :jar-file jar-file}))
+
+(defn deploy [opts]
+  (jar opts)
+  (try ((requiring-resolve 'deps-deploy.deps-deploy/deploy)
+        (merge {:installer :remote
+                :artifact jar-file
+                :pom-file (b/pom-path {:lib lib :class-dir class-dir})}
+               opts))
+       (catch Exception e
+         (if-not (str/includes? (ex-message e) "redeploying non-snapshots is not allowed")
+           (throw e)
+           (println "This release was already deployed."))))
+  opts)
